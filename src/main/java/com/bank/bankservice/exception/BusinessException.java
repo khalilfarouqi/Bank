b90f6c633efb8eeb6c97b356f89cf0de5a3103b8
khalilfarouqi <bio.khalil.f@gmail.com>
2024-03-11 16:40:49 +0000

    some changes in exception

--- src/main/java/com/bank/bankservice/exception/BusinessException.java
+++ src/main/java/com/bank/bankservice/exception/BusinessException.java
@@ -1,7 +1,7 @@
 package com.bank.bankservice.exception;
 
-public class BusinessException extends Throwable {
-    public BusinessException(String format) {
-
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
     }
 }
