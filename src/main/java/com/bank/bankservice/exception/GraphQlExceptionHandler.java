61440b755fd36d4ffcc981a1b556a95f0d3878a5
khalilfarouqi <bio.khalil.f@gmail.com>
2024-03-20 21:35:32 +0000

    add new GraphQlException

---
+++ src/main/java/com/bank/bankservice/exception/GraphQlExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.bank.bankservice.exception;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+
+public class GraphQlExceptionHandler extends DataFetcherExceptionResolverAdapter {
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        if (ex instanceof BusinessException) {
+            return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .message(ex.getMessage())
+                    .build();
+        } else {
+            return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.INTERNAL_ERROR)
+                    .message(String.format("Technical error !!. Contact your administrator : see details : [%s]", ex.getMessage()))
+                    .build();
+        }
+    }
+}
