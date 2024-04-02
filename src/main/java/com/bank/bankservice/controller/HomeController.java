05b20e66ae0fae08109556cb6f922eb5d148dad9
khalilfarouqi <bio.khalil.f@gmail.com>
2024-04-02 21:44:11 +0000

    add auth2

---
+++ src/main/java/com/bank/bankservice/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.bank.bankservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "hello, home !";
+    }
+    @GetMapping("/secured")
+    public String secured(){
+        return "hello, secured !";
+    }
+}
