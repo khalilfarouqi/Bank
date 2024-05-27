cdf4cb5c4c85144fb5afacc7525699c814e83093
khalilfarouqi <bio.khalil.f@gmail.com>
2024-05-27 22:47:22 +0100

    add new signup services

--- src/main/java/com/bank/bankservice/Services/ICustomerService.java
+++ src/main/java/com/bank/bankservice/Services/ICustomerService.java
@@ -6,6 +6,7 @@ import com.bank.bankservice.common.dtos.Response.*;
 
 public interface ICustomerService extends IService<CustomerDto> {
     AddCustomerResponse createCustomer(AddCustomerRequest addCustomerRequest);
+    AddCustomerResponse signUpCustomer(AddCustomerRequest addCustomerRequest);
     UpdateCustomerResponse updateCustomer(String identityRef, UpdateCustomerRequest updateCustomerRequest);
     CustomerDto getCustomByIdentity(String identity);
     String deleteCustomerByIdentityRef(String identityRef);
--- src/main/java/com/bank/bankservice/Services/IUserService.java
+++ src/main/java/com/bank/bankservice/Services/IUserService.java
@@ -1,6 +1,10 @@
 package com.bank.bankservice.services;
 
+import com.bank.bankservice.common.dtos.Request.AddCustomerRequest;
+import com.bank.bankservice.common.dtos.Request.AddUserRequest;
 import com.bank.bankservice.common.dtos.Request.UpdatePasswordRequest;
+import com.bank.bankservice.common.dtos.Response.AddCustomerResponse;
+import com.bank.bankservice.common.dtos.Response.AddUserResponse;
 import com.bank.bankservice.common.dtos.Response.UpdatePasswordResponse;
 import com.bank.bankservice.common.dtos.UsersDto;
 import com.bank.bankservice.entity.Users;
@@ -10,4 +14,5 @@ import java.util.Optional;
 public interface IUserService extends IService<UsersDto> {
     Optional<Users> findByUserName(String username);
     UpdatePasswordResponse updatePassword(UpdatePasswordRequest updatePasswordRequest);
+    AddUserResponse signUpUser(AddUserRequest addUserRequest);
 }
