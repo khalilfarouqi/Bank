7d33260c654ebe09f188f07261cf0d14e2301040
khalilfarouqi <bio.khalil.f@gmail.com>
2024-05-29 22:48:07 +0100

    add AuthController

---
+++ src/main/java/com/bank/bankservice/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.bank.bankservice.controller;
+
+import com.bank.bankservice.common.dtos.Request.AuthenticationRequest;
+import com.bank.bankservice.common.dtos.Response.AuthenticationResponse;
+import com.bank.bankservice.common.jwt.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
