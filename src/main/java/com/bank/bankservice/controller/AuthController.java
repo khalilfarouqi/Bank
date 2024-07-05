d5d4d903aae89e6ce53299c422eab541d39cd59c
khalilfarouqi <bio.khalil.f@gmail.com>
2024-07-05 23:38:52 +0100

    Clean code

--- src/main/java/com/bank/bankservice/controller/AuthController.java
+++ src/main/java/com/bank/bankservice/controller/AuthController.java
@@ -23,8 +23,7 @@ public class AuthController {
     @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-            );
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
         } catch (BadCredentialsException e) {
             throw new Exception("Incorrect username or password", e);
         }
